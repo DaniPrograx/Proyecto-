762d648c619d04fe1ec5e7ed0b2fcc09ad03673f
wilderhidalgomarin@gmail.com <wilderhidalgomarin@gmail.com>
2024-12-01 17:56:11 -0600

    Se realiza repositorio del proyecto

---
+++ src/proyecto_tienda_electronica/Proyecto_Tienda_Electronica.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_tienda_electronica;
+
+/**
+ *
+ * @author Admin
+ */
+public class Proyecto_Tienda_Electronica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
